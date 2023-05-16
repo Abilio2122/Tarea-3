9f7e286bad634044f413f39b2e3c3e61f67b7024
Abilio2122 <128102421+Abilio2122@users.noreply.github.com>
2023-05-16 18:08:50 -0400

    desarrolle la funcion mostrar tareas e inclui las funciones heap

---
+++ heap.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include "heap.h"
+
+typedef struct nodo{
+   void* data;
+   int priority;
+}heapElem;
+
+typedef struct Heap{
+  heapElem* heapArray;
+  int size;
+  int capac;
+} Heap;
+
+
+void* heap_top(Heap* pq){
+  if(pq->size==0){
+    return NULL;
+  }
+  else{
+    return pq->heapArray[0].data;
+  }
+}
+
+int sizeH(Heap* pq){
+  if (pq == NULL) {
+        // Si el puntero al mapa es NULL, no hay elementos
+        return 0;
+    }
+
+    return pq->size;
+}
+
+void heap_push(Heap* pq, void* data, int priority){
+  //si el arreglo esta lleno lo aumento de tamaño
+  if(pq->size==pq->capac){
+    pq->capac=(pq->capac*2)+1;
+    pq->heapArray = realloc(pq->heapArray, sizeof(heapElem)*(pq->capac));
+  }
+    //inserté el dato al ultimo espacio disponible
+    pq->heapArray[pq->size].data=data;
+    pq->heapArray[pq->size].priority=priority;
+
+  //definiendo valores para la funcion siguiente
+  heapElem aux;
+  //representa al hijo
+  int s=pq->size;
+  //representa al padre
+  int padre=(s-1)/2;
+  
+  //funcion recursiva que asegurara que reordenara todos los datos para que se cumpla el orden requerido del monticulo
+  
+  while((pq->heapArray[s].priority)<(pq->heapArray[padre].priority)){
+    if((pq->heapArray[s].priority)<(pq->heapArray[padre].priority)){ 
+      aux=pq->heapArray[s];
+      pq->heapArray[s]=pq->heapArray[padre];
+      pq->heapArray[padre]=aux;
+      s=padre;
+      padre=(s-1)/2;
+    }
+    else break;
+  }
+//actualizo el size
+pq->size++;
+}
+
+
+void heap_pop(Heap* pq){
+  //compruebo de que el heap no este vacio
+  if (pq->size == 0) {
+    return; // el heap está vacío
+  }
+  //intercambio valores de la raiz y de el ultimo dato
+  pq->heapArray[0]=pq->heapArray[pq->size];
+  pq->size--;
+  //a continuacion reordenare los datos para mantener el monticulo
+  heapElem aux;
+  for(int i=0;i<pq->capac;i++){
+    //defino los valores hijos
+    int left =2i+1;
+    int right=2i+2;
+    int posPadre=(i-1)/2;
+    //en caso de que el hijo izquierdo sea mayor que el izquierdo este se intercambiara con el padre y lo mismo en caso contrario
+    if(pq->heapArray[left].priority<pq->heapArray[right].priority){
+      aux=pq->heapArray[left];
+      pq->heapArray[left]=pq->heapArray[posPadre];
+      pq->heapArray[posPadre]=aux;
+    }
+    else{
+      aux=pq->heapArray[right];
+      pq->heapArray[right]=pq->heapArray[posPadre];
+      pq->heapArray[posPadre]=aux;
+      
+    }
+  }
+}
+
+Heap* createHeap(){
+  Heap *dato=malloc(sizeof(Heap));
+  dato->size=0;
+  dato->capac=3;
+  dato->heapArray=(heapElem *)malloc(sizeof(heapElem)*dato->capac);
+  
+  return dato;
+}
--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <string.h>
 #include "hashmap.h"
+#include "heap.h"
 #define BARRA "-------------------------------------------------------"
   
 typedef struct Tarea Tarea;
@@ -12,7 +13,7 @@ typedef struct Tarea Tarea;
 struct Tarea{
   char nombre[31];
   int prioridad;
-  List *precedencia; // duda esta lista podra apuntar a otras tareas? o solo su copia de datos?
+  List *precedencia; //esta lista guardara las claves de otras tareas
 } ;
 
 void mostrarMenu() {
@@ -28,18 +29,21 @@ void agregarTarea(HashMap * map, char nombre[], int prior){
   strcpy(nodo->nombre,nombre);
   
   nodo->prioridad=prior;
-  insertMap(map, nodo->nombre, nodo);  
+  insertMap(map, nodo->nombre, nodo);
 }
 
-//||
 void estPreced(HashMap *map, char preced[], char nombre[]){
-  // aca solo quiero agregar un precedente en una lista dentro de el struct tarea. la idea que esta lista apunte a otras tareas como precedentes y valide luego de si la tarea siga teniendo precedentes para el monticulo de la siguiente funcion.
+  // aca solo quiero agregar la clave de un precedente en una lista dentro de el struct tarea.
   Pair *casilla=searchMap(map,nombre);
   if(casilla==NULL){
     printf("no se encontro dato tarea");
     return;
   }
-  ((Tarea *)casilla->value)->precedencia=createList();
+  
+  if(((Tarea *)casilla->value)->precedencia==NULL)
+    ((Tarea *)casilla->value)->precedencia=createList();
+
+  //guardo la lista precedente en una variable por orden
   List *listPrec=((Tarea *)casilla->value)->precedencia;
 
   //busco la tarea prec
@@ -48,16 +52,57 @@ void estPreced(HashMap *map, char preced[], char nombre[]){
     printf("no se encontro dato precedente");
     return;
   }
-  //inserto esa otra tarea en la lista precedencia para que la tarea apunte a esa otra tarea
-  pushBack(listPrec,casillaPrec);
+  //inserto esa otra tarea en la lista precedencia para que la tarea guarde la clave de esa tarea
+  pushBack(listPrec,casillaPrec->key);
 }
 
-//||
+
 void mostrarTareas(HashMap * map){
-  for(Pair *i=firstMap(map);i!=NULL;i=nextMap(map)){
-    printf("%s,%d\n",i->key,((Tarea *)i->value)->prioridad);
-    //recorro lista de precedencia casilla
-    //si es null se inserta de los primeros en el heap
+  List *listRealizados=createList();
+  HashMap *mapAux=createMap(10002);
+  Heap *monMin=createHeap();
+  List *listPrec;// puede dar error
+  Pair *casillaAux;
+  Tarea *tareaLista;
+
+  while(size(map)!=0 || sizeH(monMin)!=0){
+    //recorremos el mapa
+    for(Pair *i=firstMap(map);i!=NULL;i=nextMap(map)){
+      listPrec=((Tarea *)i->value)->precedencia;
+      //en el caso de que la lista precedencia de una casilla tenga datos aka el dato tiene precedentes se verificara toda su lista para ver si algun precedente ya se hizo
+      /////////////////////////////////////////////////////
+      if(firstList(listPrec)!=NULL){
+        for (char *a = firstList(listPrec); a != NULL; a = nextList(listPrec)){
+          //compruebo en el caso de que el mapAux tenga datos
+          if(size(mapAux)){
+            casillaAux=searchMap(mapAux,a);
+            //en el caso de que la lista de precedentes se encuentre en el mapAux este se eliminará
+            if(casillaAux!=NULL){ 
+              popCurrent(listPrec);
+            }
+          }
+        }
+      }
+      ///////////////////////////////////////////////////////////
+      //en el otro caso inserto el dato directamente en el monticulo minimo
+      if(firstList(listPrec)==NULL){ 
+        heap_push(monMin,i->value,((Tarea *)i->value)->prioridad);
+        eraseMap(map,i->key);
+      }
+    }
+    // Una vez terminado el recorrido del arreglo map, se realizarán las siguientes tareas:
+        // 1. Se almacenará la raíz de monMin en listRealizados
+        // 2. Se guardará también en mapAux
+        // 3. Por último, se eliminará la raíz del monMin
+    
+    tareaLista=heap_top(monMin);
+    pushBack(listRealizados,tareaLista);
+    insertMap(mapAux,tareaLista->nombre,tareaLista);
+    heap_pop(monMin);
+  }
+  //a continuacion se mostraran los datos precedentes
+  for (Tarea *a = firstList(listRealizados); a != NULL; a = nextList(listRealizados)){
+    printf("%s %d\n", a->nombre,a->prioridad);
   }
 }
 
@@ -66,8 +111,15 @@ int main()
   HashMap *map = createMap(10002); 
   int comand, prioridad;
   char nombre[31], tarea[31], preced[31];
-  
+  // para crear un numero aleatorio
+  srand(time(NULL));  // Inicializar la semilla de generación de números aleatorios
+  int min = 1;
+  int max = 10;
+  int randomNum;
+  //////////////////////////////////////////////////////////////////////////////
   while(true){ 
+    randomNum = min + rand() % (max - min + 1); // genera randoms
+    
     mostrarMenu();
     scanf("%d",&comand);
 
@@ -79,11 +131,14 @@ int main()
     if(comand==1){
       printf("ingrese el nombre de tarea\n");
       scanf("%s",nombre);
-
-      printf("ingrese la prioridad de su tarea\n");
-      scanf("%d",&prioridad);
-      agregarTarea(map,nombre,prioridad);
       
+      
+      /*printf("ingrese la prioridad de su tarea\n");
+      scanf("%d",&prioridad);
+      */
+      
+      agregarTarea(map,nombre,randomNum);
+      printf("%d",randomNum);
     }
 
     if(comand==2){
@@ -94,7 +149,7 @@ int main()
       printf("ingrese el precedente de la tarea");
       scanf("%s",nombre);
       strcpy(preced,nombre);
-      
+
       estPreced(map,preced,tarea);
       
     }
@@ -118,6 +173,8 @@ plan para la funcion procedencia:
 -empieza por recibir un nodo de tarea 
 -asignarle a ese nodo dentro de su struct la parte de el puntero de procedencia la direccion de el nodo al que le debe la procedencia.
 
+-----------------------------------------------------------------------
+
 plan mostrar tareas
         estructuras necesitadas
         -lista de realizados
@@ -126,19 +183,41 @@ plan mostrar tareas
 -while se tienen que acabar los datos del mapa
 -for se recorre todo el mapa
 -en el caso de que no tenga precedente se insertara en el mapa auxiliar y tambien se insertara en el monticulo minimo
--en cada casilla tendra una lista que se comprobara si un dato dentro de esa lista estara en el mapa auxiliar, en el caso de que este este precedente se eliminara de la lista
+-en cada casilla tendra una lista que se comprobara si un dato dentro de esa lista estara en el mapa auxiliar, en el caso de que esté este precedente se eliminara de la lista
 -al final de el for se hara lo siguiente
 -popheap eliminara la raiz para ponerlo en la lista de realizados
 -se agregaran los datos en la lista de tareas realizadas.
 -de la lista se pone al mapa auxiliar
 
+---------------------------------------------------------------------
+
 recordatorios: 
 
 -para recorrer listas:
+
 for (Tarea *a = firstList(tareas); a != NULL; a = nextList(tareas)){
     printf("%s %d\n", a->nombre,a->prioridad);
   }
+  
+  
+-para comprobar si la funcion 1 y 2 funcionan correctamente
 
+void mostrarTareas(HashMap * map){
+  for(Pair *i=firstMap(map);i!=NULL;i=nextMap(map)){
+    printf("%s,%d\n",i->key,((Tarea *)i->value)->prioridad);
+    //recorro lista de precedencia casilla
+    for (char *a = firstList(((Tarea *)i->value)->precedencia); a != NULL; a = nextList(((Tarea *)i->value)->precedencia)){
+      printf("precedentes:");
+      printf("%s, ",a);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+
+detalles que ver:
+si hay 2 claves con el mismo nombre da error
 */
 
 
